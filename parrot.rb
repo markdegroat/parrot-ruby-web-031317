7a36deadd6720364abfc8cf64d0c288345c1c58b
Mark deGroat <mhdegroat@gmail.com>
2017-03-02 23:54:39 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(phrase = "Squawk!")
+  #because this returns nil we need to explicitly return
+  puts phrase
+  return phrase
+end
